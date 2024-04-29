4dbe5e4bd00ab706613c7948a42ca6c0bb1186a1
YukaChoco <sugar.ore.yk@gmail.com>
2024-04-29 18:20:16 +0900

    テーマカラーを変更・文字ずれ修正

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -8,19 +8,24 @@ export default function Timer({
   isStudying: boolean;
 }) {
   const colorStyles = {
-    border: `4px solid ${isStudying ? "rgb(255, 44, 79)" : "rgb(0, 0, 150)"}`,
-    color: `${isStudying ? "rgb(255, 44, 79)" : "rgb(0, 0, 150)"}`,
+    border: `4px solid ${isStudying ? "rgb(6, 73, 20)" : "rgb(6, 73, 20)"}`,
+    backgroundColor: isStudying ? "rgb(6, 73, 20)" : "rgb(192, 236, 197)",
+    color: isStudying ? "white" : "rgb(6, 73, 20)",
   };
 
   return (
     <div style={colorStyles} className={styles.timer}>
       <span>
-        {Math.floor(currentTime / 60) < 10 ? `0${Math.floor(currentTime / 60)}` : Math.floor(currentTime / 60)}
+        {Math.floor(currentTime / 600) < 10
+          ? "0"
+          : Math.floor(currentTime / 600)}
       </span>
+      <span>{Math.floor((currentTime / 60) % 10)}</span>
       <span>:</span>
       <span>
-        {currentTime % 60 < 10 ? `0${currentTime % 60}` : currentTime % 60}
+        {currentTime % 60 < 10 ? "0" : Math.floor((currentTime % 60) / 10)}
       </span>
+      <span>{(currentTime % 60) % 10}</span>
     </div>
   );
 }
