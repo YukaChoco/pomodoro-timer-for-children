4a45b745590d4a87d41efc2b836a801050e4a4e2
YukaChoco <sugar.ore.yk@gmail.com>
2024-04-29 17:34:56 +0900

    タイマーがずれないよう固定

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -1,3 +1,5 @@
+import styles from "./timer.module.css";
+
 export default function Timer({
   currentTime,
   isStudying,
@@ -5,17 +7,20 @@ export default function Timer({
   currentTime: number;
   isStudying: boolean;
 }) {
-  const styles = {
-    padding: "40px 80px",
+  const colorStyles = {
     border: `4px solid ${isStudying ? "rgb(255, 44, 79)" : "rgb(0, 0, 150)"}`,
-    fontSize: "4rem",
     color: `${isStudying ? "rgb(255, 44, 79)" : "rgb(0, 0, 150)"}`,
   };
 
   return (
-    <div style={styles}>
-      {Math.floor(currentTime / 60)}:
-      {currentTime % 60 < 10 ? `0${currentTime % 60}` : currentTime % 60}
+    <div style={colorStyles} className={styles.timer}>
+      <span>
+        {Math.floor(currentTime / 60) < 10 ? `0${Math.floor(currentTime / 60)}` : Math.floor(currentTime / 60)}
+      </span>
+      <span>:</span>
+      <span>
+        {currentTime % 60 < 10 ? `0${currentTime % 60}` : currentTime % 60}
+      </span>
     </div>
   );
 }
