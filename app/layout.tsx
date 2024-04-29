fff40f019b9c92d9609f027c146c08f4b4d5770c
YukaChoco <sugar.ore.yk@gmail.com>
2024-04-29 18:39:27 +0900

    タイマーのフォント修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bungee&family=Jersey+15+Charted&family=Mochiy+Pop+One&family=Mochiy+Pop+P+One&family=Orbitron:wght@400..900&family=Rajdhani:wght@300;400;500;600;700&family=Saira:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
